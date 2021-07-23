d2117b78d8cab36ea0a1d40bbb8bf3d71c16eab5
Laura <laura.tracierra@gmail.com>
2021-07-23 11:18:39 -0300

    rota de amount passando nos testes

--- tests/integration/recommend.test.ts
+++ tests/integration/recommend.test.ts
@@ -2,7 +2,7 @@ import '../../src/setup';
 import supertest from "supertest";
 import app from "../../src/app";
 import { clearDatabase, endConnection } from '../utils/database';
-import faker from 'faker';
+import faker, { fake } from 'faker';
 import { recommendASong } from '../factories/recommendFactory'
 
 beforeEach(async () => {
@@ -18,7 +18,7 @@ const agent = supertest(app);
 describe("GET /recommendations/random", () => {
     it("should answer with status 404 when there are no songs", async () => {
       try{
-        const response = await agent.post(`/recommendations/random`);
+        const response = await agent.get(`/recommendations/random`);
         expect(response.status).toBe(404);
       }
       catch(e){
@@ -28,7 +28,7 @@ describe("GET /recommendations/random", () => {
     it("should answer with status 201 when params are valid", async () => {
         const event = await recommendASong();
         try{
-          const response = await agent.post(`/recommendations/amount`);
+          const response = await agent.get(`/recommendations/amount`);
           expect(response.body).toEqual([event]);
           expect(response.status).toBe(201);
         }
@@ -39,5 +39,37 @@ describe("GET /recommendations/random", () => {
   });
 
   describe("GET /recommendations/top/:amount", () => {
-    
+    it("should answer with status 400 when params are empty", async () => {
+      try{
+        const response = await agent.get(`/recommendations/top/`);
+        expect(response.status).toBe(40);
+      }
+      catch(e){
+        console.error(e);
+      }
+    });
+
+    it("should answer with status 404 when params are invalid", async () => {
+        const amount: string = faker.name.title()
+        try{
+          const response = await agent.get(`/recommendations/top/${amount}`);
+          expect(response.status).toBe(404);
+        }
+        catch(e){
+          console.error(e);
+        }
+      });
+
+      it("should answer with status 201 when params are valid", async () => {
+        const amount: number = faker.datatype.number();
+        const event = await recommendASong();
+        try{
+          const response = await agent.get(`/recommendations/top/${amount}`);
+          expect(response.status).toBe(201);
+          expect(response.body).toEqual([event]);
+        }
+        catch(e){
+          console.error(e);
+        }
+      });
   });
